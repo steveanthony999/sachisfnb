9163362536fa4b68ed497f83c10d1b09886700df
Steven Woodward <steveanthony999@gmail.com>
2020-05-31 12:25:36 -0700

    landing page added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='hero'>
+        <div className='hero-left'>
+          <div className='cta'>
+            <h1>Feelin' Snackish?</h1>
+            <p>
+              Give your guests the best in high-end candies, snacks, and
+              non-alcoholic beverages
+            </p>
+            <button className='btn'>Give Me Chocolate!</button>
+          </div>
+        </div>
+        <div className='hero-right'></div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// feelin' snackish?  Give your guests the best in high-end candies, snacks, and non-alcoholic beverages!
